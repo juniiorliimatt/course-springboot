e92d10e05c0264b2a2c375171cf924d59f551304
Junior Lima <juniiorliimatt@gmail.com>
2021-07-08 09:16:49 -0300

    Project created

---
+++ src/main/java/br/com/course/CourseJavaSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJavaSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseJavaSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/course/CourseJavaSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJavaSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
