dae33c2f373725821a7307d87239bd71cf31f777
Junior Lima <juniiorliimatt@gmail.com>
2021-07-08 14:52:39 -0300

    OrderStatus Enum

--- src/main/java/br/com/course/config/TestConfig.java
+++ src/main/java/br/com/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package br.com.course.config;
 
 import br.com.course.entities.Order;
 import br.com.course.entities.User;
+import br.com.course.entities.enuns.OrderStatus;
 import br.com.course.repositories.OrderRepository;
 import br.com.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
     var u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
     userRepository.saveAll(Arrays.asList(u1, u2));
 
-    var o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-    var o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-    var o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+    var o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+    var o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+    var o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
   }
 }
